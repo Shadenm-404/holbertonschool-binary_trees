230a67c8ed2f0a63c706c1d177ef9f877af0977f
Shaden Alalwani <viuxwl7@gmail.com>
2025-10-26 06:00:16 +0000

    Fix balance: add internal bt_height helper to avoid undefined reference in checker

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,20 +1,36 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - Measures the balance factor of a binary tree
- * @tree: Pointer to the root node of the tree
+ * bt_height - Helper function to measure height of a binary tree
+ * @tree: Pointer to the node to measure height
  *
- * Return: Balance factor, or 0 if tree is NULL
+ * Return: Height or 0 if tree is NULL
  */
-int binary_tree_balance(const binary_tree_t *tree)
+static size_t bt_height(const binary_tree_t *tree)
 {
-	int left_height, right_height;
+	size_t left_h = 0, right_h = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	left_height = (tree->left) ? (int)binary_tree_height(tree->left) : -1;
-	right_height = (tree->right) ? (int)binary_tree_height(tree->right) : -1;
+	if (tree->left)
+		left_h = 1 + bt_height(tree->left);
+	if (tree->right)
+		right_h = 1 + bt_height(tree->right);
 
-	return (left_height - right_height);
+	return (left_h > right_h ? left_h : right_h);
+}
+
+/**
+ * binary_tree_balance - Measures the balance factor of a binary tree
+ * @tree: Pointer to the root node of the tree
+ *
+ * Return: Balance factor or 0 if tree is NULL
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return ((int)bt_height(tree->left) - (int)bt_height(tree->right));
 }
